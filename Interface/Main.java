d1ebdd1c802c9c84855bc3bc0a6e08366a4ebcb1
L4nex <mchadolucas@gmail.com>
2021-02-09 18:51:39 -0300

    Update Main.java

--- Interface/Main.java
+++ Interface/Main.java
@@ -3,7 +3,7 @@ package Interface;
 public class Main {
 
 	public static void main(String[] args) {
-		FigurasGeometricas fig = new FigurasGeometricas(1.0, 3.0, 4.0);
+		FigurasGeometricas figura = new FigurasGeometricas(1.0, 3.0, 4.0);
 	}
 
 }
